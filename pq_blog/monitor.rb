b8ed48733631fadc457bbb0acfb053a036b1fc7b
Colin Surprenant <colin.surprenant@gmail.com>
2017-07-18 12:35:50 -0400

    remove comments

--- pq_blog/monitor.rb
+++ pq_blog/monitor.rb
@@ -30,10 +30,9 @@ poller_thread = Thread.new do
     sleep(POLL_DELAY)
   end
 end
-
+ ddd
 stats_thread = Thread.new do
-  stat = queue.pop
-  last_stat = stat
+  last_stat = queue.pop
 
   loop do
     stat = queue.pop
@@ -44,49 +43,3 @@ stats_thread = Thread.new do
 end
 
 stats_thread.join
-
-
-# rate = 0
-
-# def poll
-#   Net::HTTP.start("localhost", 9600, {:read_timeout => 1, :open_timeout => 1}) {|http| http.get("/_node/stats")}
-# rescue Net::OpenTimeout, Net::ReadTimeout => e
-#   $stderr.puts("http request exception: #{e.inspect}")
-#   nil
-# end
-
-# def parse(response)
-#   if response.is_a?(Net::HTTPSuccess)
-#     stats = JSON.parse(response.body)
-#     return stats["pipeline"]["events"]["out"]
-#   else
-#     $stderr.puts("http response error #{response.inspect}")
-#     return nil
-#   end
-# end
-
-# response = poll()
-# last_events_out = parse(response)
-# fail("invalid first poll") if last_events_out.nil?
-
-# loop do
-#   response = poll()
-
-#   if (n = parse(response))
-#     rate = n - last_events_out
-#     last_events_out = n
-#   end
-
-
-#   last_events_out = (n.nil? ? last_events_out : n)
-
-#   if response.is_a?(Net::HTTPSuccess)
-#     stats = JSON.parse(response.body)
-#     rate = stats["pipeline"]["events"]["out"] - last_events_out if last_events_out
-#     last_events_out = stats["pipeline"]["events"]["out"]
-#   end
-
-#   puts(rate) # just output last rate to avoid graph anomalies
-
-#   sleep(1)
-# end
